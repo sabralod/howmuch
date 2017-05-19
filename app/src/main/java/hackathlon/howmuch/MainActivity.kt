c298a42d0b40aed64c46bb1cbabc61b1ccc313b9
Konstantin Seitz <konstantinseitz@live.de>
2017-05-19 22:29:58 +0200

    First commit, button test

--- app/src/main/java/hackathlon/howmuch/MainActivity.kt
+++ app/src/main/java/hackathlon/howmuch/MainActivity.kt
@@ -2,11 +2,19 @@ package hackathlon.howmuch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button1: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button1 = findViewById(R.id.button_1) as Button
+        button1.setOnClickListener { Toast.makeText(this, "button works.", Toast.LENGTH_SHORT).show() }
     }
+
+
 }
